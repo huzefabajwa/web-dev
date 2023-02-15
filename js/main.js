afe966e5deea1095a88b349ee0a2086ff331171c
huzefabajwa <huzefa_bajwa@hotmail.com>
2023-02-15 19:04:38 +0500

    commited

--- js/main.js
+++ js/main.js
@@ -4,11 +4,29 @@
 //     x=20
 //     console.log('changed inside the scope',x);
 // }
-const author='huzefa';
-const array=[1,2,3,4,5];
+const author = "huzefa";
+const array = [1, 2, 3, 4, 5];
 {
-    console.log(array);
+  console.log(array);
 }
-function performComputation(){
-    return true, 0;
+// function performComputation(){
+//     return true, 0;
+// }
+const performComputation = () => {
+  return { result: 42, error: NULL };
+};
+
+function setname(name){
+    console.log(name);
 }
+setname(()=>{return "john"});
+(
+    sfu = () => {
+        console.log('nameless');
+    }
+)();
+(
+    function sfu(){
+        console.log('nameless');
+    }
+)
