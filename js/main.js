359ee33af12f0b94e49a0b938fbff22deeac6507
huzefabajwa <huzefa_bajwa@hotmail.com>
2023-02-15 18:50:26 +0500

    "commit"

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-window.prompt();
+// var x=10
+// {
+//     console.log('original value', x)
+//     x=20
+//     console.log('changed inside the scope',x);
+// }
+const author='huzefa';
+const array=[1,2,3,4,5];
+{
+    console.log(array);
+}
+function performComputation(){
+    return true, 0;
+}
